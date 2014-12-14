5f8a1e4e74bf673180402ed478e1d675b91deb63
Adrien Nhem <adrien.nhem@gmail.com>
2014-12-14 17:08:42 +0000

    redirect to homepage when get to invalid cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :line_items
   resources :products
   resources :carts
-
+  get 'store' => 'store#index'
   root 'store#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
