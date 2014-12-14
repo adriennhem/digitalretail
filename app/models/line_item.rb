445f328a61e81f18e4014be038cff78cfb348760
Adrien Nhem <adrien.nhem@gmail.com>
2014-12-14 18:06:44 +0000

    added total to the cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,9 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
   validates :product_id, presence: true
+
+  def total_price 
+		product.price * quantity
+	end
+	
 end
